d82404bb0f7631e66e4b009c3ea47ea8499cd6ac
Eric Wait <ericwait@drexel.edu>
2014-07-10 15:34:15 -0400

    Reset view as a temp bug fix

--- src/matlab/lever3d.m
+++ src/matlab/lever3d.m
@@ -10,8 +10,11 @@ useDistance = 0;
 [arrowFaces, arrowVerts, arrowNorms] = MakeArrow(0.65,0.05,0.15,40);
 [sphereFaces, sphereVerts, shereNorms] = MakeSphere(0.20,40);
 
+[FileName,PathName,~] = uigetfile('.txt');
+if (FileName==0), return, end
+
 %[orgImage, imageData] = tiffReader([],[],[],[],'d:\Users\Eric.Bioimage29\Documents\Images\LEVer3d\Susan_overnight_new');
-[orgImage, imageData] = tiffReader();
+[orgImage, imageData] = tiffReader([],[],[],[],fullfile(PathName,FileName));
 
 processedImage = orgImage;
 
@@ -22,6 +25,6 @@ lever_3d('init',arrowFaces, arrowVerts, arrowNorms,sphereFaces, sphereVerts, she
 
 lever_3d('loadTexture',imageConvert(orgImage,'uint8'),[imageData.XPixelPhysicalSize,imageData.YPixelPhysicalSize,imageData.ZPixelPhysicalSize]);
 
- uiControlFig = ViewerControls();
-
+uiControlFig = ViewerControls();
+lever_3d('resetView');
 end
