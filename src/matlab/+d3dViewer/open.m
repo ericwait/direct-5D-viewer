f34c8a0b494cc1045da4d074c2c9694950ec92d7
Eric Wait <eric@waitphoto.com>
2015-09-18 13:03:22 -0400

    Open command now creates the widget from the general-matlab-tools repo

--- src/matlab/+d3dViewer/open.m
+++ src/matlab/+d3dViewer/open.m
@@ -4,6 +4,9 @@ function [ output_args ] = open( input_args )
 
 [arrowFaces, arrowVerts, arrowNorms] = MakeArrow(0.65,0.05,0.15,40);
 [sphereFaces, sphereVerts, shereNorms] = MakeSphere(0.20,40);
+%% make a widget to show the orentation of the current view in 3D
+[arrowFaces, arrowVerts, arrowNorms] = Ploygons.makeArrow(0.65,0.05,0.15,40);
+[sphereFaces, sphereVerts, shereNorms] = Ploygons.makeSphere(0.20,40);
 
 d3dViewer.d3dViewer('init',arrowFaces, arrowVerts, arrowNorms,sphereFaces, sphereVerts, shereNorms);
 end
