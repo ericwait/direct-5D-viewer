88fcda79be5930384baad0be900e4450cfb36753
Eric Wait <eric@waitphoto.com>
2015-11-03 14:44:50 -0500

    Update time does not decrement, it is done in C as well

--- src/matlab/+D3d/+UI/+Ctrl/UpdateTime.m
+++ src/matlab/+D3d/+UI/+Ctrl/UpdateTime.m
@@ -29,7 +29,7 @@ if (~isempty(D3dUICtrlHandles))
     set(D3dUICtrlHandles.handles.s_curFrame,'Value', time);
     
     if (~exist('fromC','var') || isempty(fromC) || fromC~=1)
-        D3d.Viewer('setFrame',time-1);
+        D3d.Viewer('setFrame',time);
     end
 end
 end
