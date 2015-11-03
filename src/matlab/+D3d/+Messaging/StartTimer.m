f124034c5cd7bf5876d10ba293a27697cabaabf4
Eric Wait <eric@waitphoto.com>
2015-11-03 13:48:37 -0500

    Start timer only if it is not started already

--- src/matlab/+D3d/+Messaging/StartTimer.m
+++ src/matlab/+D3d/+Messaging/StartTimer.m
@@ -5,6 +5,8 @@ if (isempty(d3dtimer) || ~isvalid(d3dtimer))
     d3dtimer = timer('TimerFcn',@D3d.Messaging.Check,'ExecutionMode','fixedSpacing','Period',0.1,'Name','d3dtimer');
 end
 
-start(d3dtimer);
+if (~strcmpi(get(d3dtimer,'Running'),'on'))
+    start(d3dtimer);
+end
 end
 
