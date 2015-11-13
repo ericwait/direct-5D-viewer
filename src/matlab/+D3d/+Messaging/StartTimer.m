5b6b8ed84175bfb8e07addc41f6459e2c2cf1242
Eric Wait <eric@waitphoto.com>
2015-11-13 14:02:53 -0500

    Slow the timer down a bit to see if the queue can be more well behaved.

--- src/matlab/+D3d/+Messaging/StartTimer.m
+++ src/matlab/+D3d/+Messaging/StartTimer.m
@@ -2,7 +2,7 @@ function StartTimer()
 global d3dtimer
 
 if (isempty(d3dtimer) || ~isvalid(d3dtimer))
-    d3dtimer = timer('TimerFcn',@D3d.Messaging.Check,'ExecutionMode','fixedSpacing','Period',0.1,'Name','d3dtimer');
+    d3dtimer = timer('TimerFcn',@D3d.Messaging.Check,'ExecutionMode','fixedSpacing','Period',0.5,'Name','d3dtimer');
 end
 
 if (~strcmpi(get(d3dtimer,'Running'),'on'))
