8423df334b837aa3194226e763b7365e0dd60cff
Eric Wait <eric@waitphoto.com>
2016-07-06 14:26:10 -0400

    Implemented ShowViewOrigin

--- src/c/Mex/MexSetViewOrigin.cpp
+++ src/c/Mex/MexSetViewOrigin.cpp
@@ -5,9 +5,7 @@
 void MexSetViewOrigin::execute(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) const
 {
 	double* origin = (double*)mxGetData(prhs[0]);
-	size_t numDims = mxGetNumberOfElements(prhs[10]);
-
-	if (numDims != 3) mexErrMsgTxt("There needs to be three doubles for the view origin!");
+	size_t numDims = mxGetNumberOfElements(prhs[0]);
 
 	double* originMsg = new double[3];
 	originMsg[0] = origin[0];
@@ -19,20 +17,26 @@ void MexSetViewOrigin::execute(int nlhs, mxArray* plhs[], int nrhs, const mxArra
 
 std::string MexSetViewOrigin::check(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) const
 {
-	if (nrhs != 2)
+	if (nrhs != 1)
 		return "Not the right arguments for setViewOrigin!";
+
+	double* origin = (double*)mxGetData(prhs[0]);
+	size_t numDims = mxGetNumberOfElements(prhs[0]);
+
+	if(numDims!=3) 
+		return "There needs to be three doubles for the view origin!";
 
 	return "";
 }
 
 void MexSetViewOrigin::usage(std::vector<std::string>& outArgs, std::vector<std::string>& inArgs) const
 {
-	//inArgs.push_back("arrowFaces");
+	inArgs.push_back("viewOrigin");
 }
 
 void MexSetViewOrigin::help(std::vector<std::string>& helpLines) const
 {
-	//helpLines.push_back("This initializes the D3d viewing window.  It takes a widget to show orientation. The ARROW will be the polygons that point down each axis.  The SPHERE is the center polygon that is centered at the (widget's) axis origin.");
+	helpLines.push_back("This will set the view origin (where the camera looks).");
 
-	//helpLines.push_back("\tArrowFaces -- This is an ordered list of vertices that make up the facets (triangles) for each axis.");
+	helpLines.push_back("\tViewOrigin - this is three values that correspond to the point that you would like the camera to point at.");
 }
--- src/matlab/+D3d/@Viewer/SetViewOrigin.m
+++ src/matlab/+D3d/@Viewer/SetViewOrigin.m
@@ -1,4 +1,6 @@
-% SetViewOrigin - SetViewOrigin() 
-function SetViewOrigin()
-    D3d.Viewer.Mex('SetViewOrigin');
+% SetViewOrigin - This will set the view origin (where the camera looks).
+%    Viewer.SetViewOrigin(viewOrigin)
+%    	ViewOrigin - this is three values that correspond to the point that you would like the camera to point at.
+function SetViewOrigin(viewOrigin)
+    D3d.Viewer.Mex('SetViewOrigin',viewOrigin);
 end
