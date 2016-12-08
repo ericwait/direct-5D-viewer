b7ef6140c480b0eefda3898634c06dd9cef71a50
Mark Winter <mwinter@drexel.edu>
2016-12-08 15:44:44 -0500

    Build script now copies the shaders folder into @VIewer.

--- src/matlab/+D3d/BuildScript.m
+++ src/matlab/+D3d/BuildScript.m
@@ -2,4 +2,8 @@ curPath = pwd();
 D3dPath = fileparts(which('D3d.Open'));
 cd(D3dPath)
 D3d.BuildMexObject('..\..\c\Mex.mexw64','Viewer');
+if ( ~exist('@Viewer/Shaders','dir') )
+    mkdir('@Viewer/Shaders');
+end
+copyfile('../../c/Shaders/','@Viewer/Shaders/');
 cd(curPath)
