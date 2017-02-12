72ba01bad0f09435a5e13826f30ab2528a9bf652
Eric Wait <eric@waitphoto.com>
2017-02-12 13:10:47 -0500

    Changed the Open and Load Image paradigm.

--- src/matlab/+D3d/LoadImage.m
+++ src/matlab/+D3d/LoadImage.m
@@ -1,116 +1,36 @@
-function [varargout] = LoadImage( im, imData, imagePath, bufferNum )
-%LOADIMAGE [varargout] = D3d.LoadImage( im, imData, imagePath, bufferNum )
-%sends the image data to the directX viewer.
-%
-%IM is an 'uint8' image 3D-5D image matrix, optional.
-%IM_DATA is astructure that holds metadata for the image and
-%   needs to be in the format that tiffReader would return, optional.
-%IMAGE_PATH is a path to the metadata file that can be read by
-%   tiffReader, optional.
+%LOADIMAGE D3d.LoadImage( im, bufferNum, frameNumber)
 
-global D3dIsOpen
-
-%% check the optional arguments and set the non-existant ones to empty
-if (~exist('im','var'))
-    im = [];
-end
-if (~exist('imData','var'))
-    imData = [];
-end
-if (~exist('imagePath','var'))
-    if (isempty(imData) || ~isfield(imData,'imageDir') )
-        imagePath = [];
+function LoadImage( im, bufferNum, frameNumber )
+    if (~exist('bufferNum','var') || isempty(bufferNum))
+        bufferNum = 1;
     else
-        imagePath = imData.imageDir;
-    end
-end
-if (~exist('bufferNum','var') || isempty(bufferNum))
-    bufferNum = 1;
-else
-    if (~bufferNum==1 && ~bufferNum==2)
-        error('bufferType can only be 1 or 2!');
-    end
-end
-
-if (isempty(D3dIsOpen) || ~D3dIsOpen)
-    [im,imData] = D3d.Open(im, imData, imagePath);
-    return
-end
-
-bufferType = 'original';
-if (bufferNum==2)
-    bufferType = 'processed';
-end
-
-%% check for missing data that we might need and load
-if (isempty(imData))
-    % there is no image data passed in to load
-    if (isempty(im))
-        % there is also no image to load, so we should read one in
-%         imData = MicroscopeData.ReadMetadata(imagePath,true);
-        % ac changed !!!
-        imData = MicroscopeData.ReadMetadata(imagePath);
-        if (isempty(imData))
-            return
+        if (~bufferNum==1 && ~bufferNum==2)
+            error('bufferType can only be 1 or 2!');
         end
-        imagePath = imData.imageDir;
-    else
-        % there is an image to load just no metadata to go with it
-        % assume that the voxels are isomorphic
-        imData.DatasetName = sprintf('%d-D image',ndims(im));
-        imSz = size(im);
-        imData.Dimensions = Utils.SwapXY_RC(imSz(1:3));
-        imData.NumberOfChannels = size(im,4);
-        imData.NumberOfFrames = size(im,5);
-        imData.PixelPhysicalSize = [1.0,1.0,1.0];
-        imData.imageDir = '.';
     end
-end
-
-%% open the missing image if it is small enough
-if (all(imData.Dimensions<=2048) && isempty(im))
-    [im,imData] = MicroscopeData.Reader('imageData',imData,'verbose',true);
-        if (isempty(im))
-                return
-        end        
-elseif (isempty(im) || any(imData.Dimensions>2048))
-    D3d.UI.InitializeMipFigure(im,imData,imData.imageDir,true);
-    imData = [];
-    im = [];
-end
-
-%% send the image to the viewer
-if (~isempty(im) && ~isempty(imData))
-    % these structures could still be empty if the user cancelled the image
-    % reading
     
+    if (~exist('frameNumber','var'))
+        frameNumber = [];
+    elseif (numel(frameNumber)>1)
+        error('You can only load one frame at a time!');
+    end
+
+    bufferType = 'original';
+    if (bufferNum==2)
+        bufferType = 'processed';
+    end
+   
     % if this is a 2D image, we will put time in the z dimension
-    if (imData.Dimensions(3)==1)
+    if (size(im,3)==1 && ndims(im)>2)
         im = permute(im,[1,2,5,4,3]);
-        imData.Dimensions(3) = imData.NumberOfFrames;
-        imData.NumberOfFrames = 1;
-        if (length(imData.PixelPhysicalSize)<3)
-            imData.PixelPhysicalSize(3) = max(imData.PixelPhysicalSize([1,2]));
-        end
     end
     
-    if (~strcmpi('uint8',class(im)))
-        im8 = ImUtils.ConvertType(im,'uint8',true);
+    im8 = ImUtils.ConvertType(im,'uint8',true);
+    if (isempty(frameNumber))
+        D3d.Viewer.LoadTexture(im8,bufferType);
     else
-        im8 = im;
+        D3d.Viewer.LoadTextureFrame(im8,frameNumber,bufferType);
     end
-    
-    D3d.Viewer.InitVolume(size(im8), imData.PixelPhysicalSize);
-    D3d.Viewer.LoadTexture(im8,bufferType);
     
     D3d.UI.Ctrl.EnableBuffer(bufferNum);
 end
-
-if (nargout>1)
-    varargout{2} = imData;
-end
-if (nargout>0)
-    varargout{1} = im;
-end
-end
-
--- src/matlab/+D3d/Open.m
+++ src/matlab/+D3d/Open.m
@@ -14,53 +14,91 @@ function [varargout] = Open( im, imData, imagePath, mesagePkgStr )
 % d3dtimer - makes a timer that will call checkMessage
 %*******************************************************************
 
-global EXT_MESAGE_FUNC D3dIsOpen
+    global EXT_MESAGE_FUNC D3dIsOpen
 
-%% check the optional arguments and set the non-existant ones to empty
-if (~exist('im','var'))
-    im = [];
-end
-if (~exist('imData','var'))
-    imData = [];
-end
-if (~exist('imagePath','var'))
-    imagePath = [];
-end
-if (~exist('mesagePkgStr','var'))
-    EXT_MESAGE_FUNC = [];
-else
-    EXT_MESAGE_FUNC = mesagePkgStr;
-end
-if (isempty(D3dIsOpen))
-    D3dIsOpen = false;
-end
+    %% check the optional arguments and set the non-existant ones to empty
+    if (~exist('im','var'))
+        im = [];
+    end
+    if (~exist('imData','var'))
+        imData = [];
+    end
+    if (~exist('imagePath','var'))
+        imagePath = [];
+    end
+    if (~exist('mesagePkgStr','var'))
+        EXT_MESAGE_FUNC = [];
+    else
+        EXT_MESAGE_FUNC = mesagePkgStr;
+    end
+    if (isempty(D3dIsOpen))
+        D3dIsOpen = false;
+    end
+    
+    %% check for missing data that we might need and load
+    if (isempty(imData))
+        % there is no image data passed in to load
+        if (isempty(im))
+            % there is also no image to load, so we should read one in
+            imData = MicroscopeData.ReadMetadata(imagePath,true);
+            if (isempty(imData))
+                return
+            end
+            imagePath = imData.imageDir;
+        else
+            % there is an image to load just no metadata to go with it
+            % assume that the voxels are isomorphic
+            imData.DatasetName = sprintf('%d-D image',ndims(im));
+            imSz = size(im);
+            imData.Dimensions = Utils.SwapXY_RC(imSz(1:3));
+            imData.NumberOfChannels = size(im,4);
+            imData.NumberOfFrames = size(im,5);
+            imData.PixelPhysicalSize = [1.0,1.0,1.0];
+            imData.imageDir = '.';
+        end
+    end
 
-if (~D3dIsOpen)
-    %% start the viewer dll
-    [pathstr,~,~] = fileparts(which('D3d.Viewer'));
-    D3d.Viewer.Init(pathstr);
-    D3dIsOpen = true;
-end
+    %% open the missing image if it is small enough
+    if (all(imData.Dimensions<=2048) && isempty(im))
+        [im,imData] = MicroscopeData.Reader('imageData',imData,'verbose',true);
+            if (isempty(im))
+                    return
+            end        
+    elseif (isempty(im) || any(imData.Dimensions>2048))
+        D3d.UI.InitializeMipFigure(im,imData,imData.imageDir,true);
+        imData = [];
+        im = [];
+    end
+    
+    %% if there is no metadata, get out without starting anything
+    if (isempty(imData))
+        if (nargout>1)
+            varargout{2} = [];
+        end
+        if (nargout>0)
+            varargout{1} = im;
+        end
+    end
 
-%% check if there should be an image loaded right away.
-[im,imData] = D3d.LoadImage(im, imData, imagePath);
+    %% start the viewer
+    if (~D3dIsOpen)
+        [pathstr,~,~] = fileparts(which('D3d.Viewer'));
+        D3d.Viewer.Init(pathstr);
+        D3dIsOpen = true;
+    end
 
-if (~isempty(im))
+    D3d.Viewer.InitVolume(size(im),imData.PixelPhysicalSize([2,1,3]));
+    D3d.LoadImage(im, imagePath);
     D3d.UI.Controls(imData);
     
     %% start a timer that will check for any messages that the viewer might want to return
     D3d.Messaging.StartTimer();
-else
-    D3d.Close();
-    D3dIsOpen = false;
-end
 
-%% return the image and data that might have been read in durring the load
-if (nargout>1)
-    varargout{2} = imData;
+    %% return the image and data that might have been read in durring the load
+    if (nargout>1)
+        varargout{2} = imData;
+    end
+    if (nargout>0)
+        varargout{1} = im;
+    end
 end
-if (nargout>0)
-    varargout{1} = im;
-end
-end
-
