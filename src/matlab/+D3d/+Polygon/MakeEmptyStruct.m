54402eac1dcc8c3f724662d231d9988aa15b947a
Eric Wait <eric@waitphoto.com>
2017-03-28 19:57:48 -0400

    Making empty poly structs will not return an empty struct

--- src/matlab/+D3d/+Polygon/MakeEmptyStruct.m
+++ src/matlab/+D3d/+Polygon/MakeEmptyStruct.m
@@ -1,6 +1,6 @@
 function [ poly ] = MakeEmptyStruct()
 %[ poly ] = D3d.Polygon.MakeEmptyStruct()
 
-poly = struct('index',{0}, 'frame',{0}, 'label',{''}, 'color',{[0,0,0]}, 'faces',{[]}, 'verts',{[]}, 'norms',{[]}, 'CenterOfMass',{[0,0,0]});
+poly = struct('index',{}, 'frame',{}, 'label',{}, 'color',{}, 'faces',{}, 'verts',{}, 'norms',{}, 'CenterOfMass',{});
 end
 
