988dfef5b2d4e67f757a84c4f49ff4bb6f9d58b9
Eric Wait <eric@waitphoto.com>
2020-10-26 08:53:42 -0400

    Added a maximum volume to the polygon maker from bwimage

--- src/matlab/+D3d/+Polygon/MakeBW.m
+++ src/matlab/+D3d/+Polygon/MakeBW.m
@@ -1,44 +1,62 @@
-function polygons = MakeBW(imBW, colors, minVol, verbose)
+function polygons = MakeBW(imBW, colors, minVol, maxVol, verbose)
     if (~exist('colors','var') || isempty(colors))
         colors = hsv(size(imBW,4));
     end
     if (~exist('minVol','var') || isempty(minVol))
         minVol = 3;
     end
+    if (~exist('maxVol','var') || isempty(maxVol))
+        maxVol = inf;
+    end
     if (~exist('verbose','var') || isempty(verbose))
         verbose = true;
     end
 
-    polygons = D3d.Polygon.MakeEmptyStruct();
+    polycell = cell(size(imBW,5),1);
     frameSize = size(imBW(:,:,:,1,1));
     
     if (verbose)
         prgs = Utils.CmdlnProgress(size(imBW,5),true,'Making Polygons',false);
     end
     
-    for t=1:size(imBW,5)
+    parfor t=1:size(imBW,5)
+        curPolys = D3d.Polygon.MakeEmptyStruct();
         for c=1:size(imBW,4)
             cc = bwconncomp(imBW(:,:,:,c,t));
             
             for ccind = 1:length(cc.PixelIdxList)
-                if (length(cc.PixelIdxList{ccind})<minVol)
+                if (length(cc.PixelIdxList{ccind})<minVol || length(cc.PixelIdxList{ccind})>maxVol)
                     continue;
                 end
                 
                 pixList_xyz = Utils.SwapXY_RC(Utils.IndToCoord(frameSize,cc.PixelIdxList{ccind}));
-                ind = length(polygons) +1;
+                ind = length(curPolys) +1;
                 ply = D3d.Polygon.Make(pixList_xyz,ind,num2str(ind),t,colors(c,:));
                 
                 if (isempty(ply))
                     continue
                 end
                 
-                polygons = vertcat(polygons,ply);
+                curPolys = vertcat(curPolys,ply);
             end
         end
         
+        polycell{t} = curPolys;
+        
         if (verbose)
             prgs.PrintProgress(t);
+        end
+    end
+    
+    polygons = D3d.Polygon.MakeEmptyStruct();
+    
+    for t=1:size(imBW,5)
+        curPolys = polycell{t};
+        for i=1:length(curPolys)
+            cp = curPolys(i);
+            cp.index = length(polygons)+1;
+            cp.label = num2str(cp.index);
+            polygons(cp.index) = cp;
         end
     end
     
